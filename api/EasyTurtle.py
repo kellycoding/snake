31ca85e1da182ca3cd3995a81fc6712eaa15e7f6
Evelyn <Evelyn@gmail.com>
2019-07-27 14:42:19 -0400

    added backend folder 'api'

---
+++ api/EasyTurtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.mainloop()
+
