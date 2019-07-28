25ab4f28243a350a526d85077266424fcf113139
kellyli123 <kellyli838@gmail.com>
2019-07-28 13:57:31 -0400

    finished basic snake game

--- ui/snake.js
+++ ui/snake.js
@@ -13,6 +13,11 @@ ground.src = "img/ground.png";
 const foodImage = new Image();
 foodImage.src = "img/food.png";
 
+// load audio files
+const dead = new Audio();
+
+dead.src = "audio/dead.mp3"
+
 //create the snake
 
 let snake = [];
@@ -32,6 +37,35 @@ let food = {
 
 let score = 0;
 
+//control the snake
+
+let d;
+
+document.addEventListener("keydown",direction)
+
+function direction(event){
+	let key = event.keyCode
+	if(key == 37 && d != "RIGHT"){
+		d = "LEFT";
+	}else if(key == 38 && d != "DOWN"){
+		d = "UP";
+	}else if(key == 39 && d != "LEFT"){
+		d = "RIGHT";
+	}else if(key == 40 && d != "UP"){
+		d = "DOWN";
+	}
+}
+
+// check collision function
+function collision(head,array){
+	for(let i = 0; i < array.length; i++){
+		if(head.x == array[i].x && head.y == array[i].y){
+			return true;
+		}
+	}
+	return false;
+}
+
 //draw everything to the canvas
 
 function draw(){
@@ -46,7 +80,50 @@ function draw(){
 		ctx.strokeRect(snake[i].x,snake[i].y,box,box);
 	}
 
-	ctx.drawImage(foodImg, food.x, food.y)
+	ctx.drawImage(foodImage, food.x, food.y)
+
+	// old head position
+	let snakeX =snake[0].x;
+	let snakeY =snake[0].y;
+
+	//which direction
+	if(d == "LEFT") snakeX -= box;
+	if(d == "UP") snakeY -= box;
+	if(d == "RIGHT") snakeX += box;
+	if(d == "DOWN") snakeY += box;
+
+	//if the snake eats the food
+	if(snakeX == food.x && snakeY == food.y){
+		score++;
+		food = {
+			x : Math.floor(Math.random()*17+1) * box,
+			y : Math.floor(Math.random()*15+3) * box
+		}
+		//we don't remove its tail
+	}else{
+		//remove the tail
+		snake.pop();
+	}
+
+	//add new head
+
+	let newHead = {
+		x : snakeX,
+		y: snakeY
+	}
+
+	//game over
+
+	if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+		clearInterval(game);
+		dead.play();
+	}
+
+	snake.unshift(newHead);
+
+	ctx.fillStyle = "white";
+	ctx.font = "45px Changa One";
+	ctx.fillText(score,2*box,1.6*box);
 }
 
 //call draw function every 100 ms
