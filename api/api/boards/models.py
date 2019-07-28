dc550bf4f7fad1a229531b1f91c65df1bcb1e9a1
Evelyn <Evelyn@gmail.com>
2019-07-28 16:54:04 -0400

    setup django server

---
+++ api/api/boards/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# 汉字难度等级.
+class Level(models.Model)
+	name = models.CharField(max_length=20, unique=True)
+	description = models.CharField(max_length=200)
+
+# 汉字库.
+class Word(models.Model)
+	name = models.CharField(max_length=20, unique=True)
+	level = models.ForeignKey(Level, related_name='names')
+
+
