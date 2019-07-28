9973ebfe8ebc75c70afee7e94bbcc38f61288ab9
Evelyn <Evelyn@gmail.com>
2019-07-28 17:02:44 -0400

    created word and level table

--- api/api/boards/models.py
+++ api/api/boards/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # 汉字难度等级.
-class Level(models.Model)
+class Level(models.Model):
 	name = models.CharField(max_length=20, unique=True)
 	description = models.CharField(max_length=200)
 
 # 汉字库.
-class Word(models.Model)
+class Word(models.Model):
 	name = models.CharField(max_length=20, unique=True)
-	level = models.ForeignKey(Level, related_name='names')
+	level = models.ForeignKey(Level, related_name='names', null=True, on_delete=models.SET_NULL)
 
 
