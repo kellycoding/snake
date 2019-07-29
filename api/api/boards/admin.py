5811f191176ded27683ed8e2840edbca9ae10141
Evelyn <Evelyn@gmail.com>
2019-07-29 09:00:54 -0400

    updated admin display

--- api/api/boards/admin.py
+++ api/api/boards/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Level, Proficiency, Word, Homograph, Sentence
 
-admin.site.register(Level)
-admin.site.register(Proficiency)
-admin.site.register(Word)
-admin.site.register(Homograph)
-admin.site.register(Sentence)
+class LevelAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'description')
+admin.site.register(Level, LevelAdmin)
+
+class ProficiencyAdmin(admin.ModelAdmin):
+	list_display = ('id', 'count', 'description')
+admin.site.register(Proficiency, ProficiencyAdmin)
+
+class WordAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'level', 'proficiency')		
+admin.site.register(Word, WordAdmin)
+
+class HomegraphAdmin(admin.ModelAdmin):
+	list_display = ('id', 'word', 'name')
+admin.site.register(Homograph, HomegraphAdmin)
+
+class SentenceAdmin(admin.ModelAdmin):
+	list_display = ('id', 'sentence')
+admin.site.register(Sentence, SentenceAdmin)
 
